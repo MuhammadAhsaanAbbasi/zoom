865c138793646cb49516e7981cd8f686a49cbff3
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-26 00:03:26 +0500

    icons

--- src/components/Navigation_menu/Navbar.tsx
+++ src/components/Navigation_menu/Navbar.tsx
@@ -1,10 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <section>
-      
-    </section>
+    <nav className='flex'>
+      <Link href={"/"}
+      className='flex'
+      >
+        <Image
+        src={"/icons/logo.svg"}
+        alt="LOOM LOGO"
+        height={40}
+        width={40}
+        className='cursor-pointer'
+        />
+        <p className="text-[30px] font-semibold">LOOM</p>
+      </Link>
+    </nav>
   )
 }
 
