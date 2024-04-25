bd9c3f2685e0b86b4184c0c055957c422505a788
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-25 19:27:42 +0500

    init commit

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -4,11 +4,11 @@ import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 
-
 export const metadata: Metadata = {
   title: 'LOOM',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
+
 
 const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
   return (
