cc79ce6c01102d9e6cf11e132e31409111fe79e5
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-26 15:09:13 +0500

    config

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,13 +1,27 @@
+import MeetingList from "@/components/Meeting/MeetingList";
 import Image from "next/image";
 
 export default function Home() {
+  const now = new Date();
+  const time = now.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})
+  const date = (new Intl.DateTimeFormat([], {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })).format(now)
   return (
     <section className="flex size-full flex-col gap-10">
       <div className="h-[303px] w-full bg-hero bg-cover rounded-[20px]">
-        <div className="">
-          <h2 className="glassmorphism max-w-[270px] text-base text-center">Upcoming Meeting at: 12:30 PM</h2>
+        <div className="flex h-full flex-col justify-between lg:p-11 max-md:px-4 max-md:py-6">
+          <h2 className="glassmorphism max-w-[270px] text-base text-center font-normal rounded">Upcoming Meeting at: 12:30 PM</h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-7xl font-bold">{time}</h1>
+            <h2 className="text-3xl font-medium text-sky-1">{date}</h2>
+          </div>
         </div>
       </div>
+      <MeetingList />
     </section>
   );
 }
