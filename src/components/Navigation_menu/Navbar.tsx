e1594dfa9d23c3d33970e8d3c457ddfc7eb95a4c
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-26 09:31:31 +0500

    sign-in & sign-up

--- src/components/Navigation_menu/Navbar.tsx
+++ src/components/Navigation_menu/Navbar.tsx
@@ -2,31 +2,42 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
     <nav className='flex flex-between bg-dark-2 fixed w-full px-6 py-4 lg:px-10 z-50'>
       <Link href={"/"}
-      className='flex items-center gap-2'
+        className='flex items-center gap-2'
       >
         <Image
-        src={"/icons/logo.svg"}
-        alt="LOOM LOGO"
-        height={40}
-        width={40}
-        className='cursor-pointer max-sm:size-10'
+          src={"/icons/logo.svg"}
+          alt="LOOM LOGO"
+          height={40}
+          width={40}
+          className='cursor-pointer max-sm:size-10'
         />
         <Image
-        src={"/icons/loom.svg"}
-        alt="LOOM LOGO"
-        height={70}
-        width={80}
-        className='cursor-pointer size-[80px] font-semibold max-sm:hidden'
+          src={"/icons/loom.svg"}
+          alt="LOOM LOGO"
+          height={70}
+          width={80}
+          className='cursor-pointer size-[80px] font-semibold max-sm:hidden'
         />
       </Link>
       <div className='flex-between gap-5'>
-        <p>Custom Auth</p>
-        <MobileNav/>
+        <UserButton
+          afterSignOutUrl='/'
+          appearance={{
+            elements: {
+              button: {
+                width: '40px', // Set the width to 100px
+                height: '40px', // Set the height to 150px
+              },
+            },
+          }}
+        />
+        <MobileNav />
       </div>
     </nav>
   )
