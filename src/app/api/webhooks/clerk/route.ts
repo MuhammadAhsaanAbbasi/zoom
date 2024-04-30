b3782a7891ce8a3e8a69ce76089ac0140c470885
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-30 08:51:27 +0500

    database

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,4 +1,5 @@
-import { clerkClient } from "@clerk/nextjs";
+// @ts-nocheck
+import { clerkClient } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
