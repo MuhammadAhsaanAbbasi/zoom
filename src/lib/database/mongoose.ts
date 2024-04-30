e67af5b631008c81663a99f37090772058837424
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-04-30 09:08:45 +0500

    database webhook clerk

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
     cached.promise =
         cached.promise ||
         mongoose.connect(MONGODB_URL, {
-            dbName: 'lensai', bufferCommands: false
+            dbName: 'loom', bufferCommands: false
         })
 
     cached.conn = await cached.promise;
