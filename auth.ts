977e2c6664009b30995890ee9f58b56bba01006d
unknown <mahsaanabbasi@gmail.com>
2025-04-04 17:04:59 +0500

    auth

--- auth.ts
+++ auth.ts
@@ -2,7 +2,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Prisma from "next-auth/providers/email";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [GitHub, Google],
+    providers: [GitHub, Google, Prisma],
 });
